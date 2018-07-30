2661f7e77373ab04e663eecdb388b0665dd172c0
Jonathan Boß <jonafred.boss@gmail.com>
2018-07-30 17:48:58 +0200

    Added methods to handle exchange.

--- QFEShortener/src/webserver/AdminHandler.java
+++ QFEShortener/src/webserver/AdminHandler.java
@@ -1,5 +1,6 @@
 package webserver;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.Scanner;
@@ -18,221 +19,103 @@ public class AdminHandler implements HttpHandler {
 		String request = ex.getRequestURI().toString().replace("/", "");
 		if (!request.equalsIgnoreCase("favicon.ico")) {
 			if (request.equalsIgnoreCase("admin")) {
-				Scanner sc = new Scanner(FileManager.getAdminFile());
-				String response = "";
-				while (sc.hasNextLine()) {
-					response = response + sc.nextLine();
-				}
-				response = response.replace("%urls%",
+				sendExchange(ex, FileManager.getAdminFile(), "%urls%",
 						Main.getURLManager().getURLSadded() + " / " + Main.getConfiguration().getURLlimit());
-				ex.sendResponseHeaders(200, response.length());
-				OutputStream os = ex.getResponseBody();
-				os.write(response.getBytes());
-				os.close();
-				sc.close();
 			} else if (request.startsWith("admin")) {
 				String requestRaw = request.replaceFirst("admin", "");
 				if (requestRaw.startsWith("localdomain=")) {
 					Main.getConfiguration().setLocalDomain(requestRaw.replace("localdomain=", ""));
-					Scanner sc = new Scanner(FileManager.getInformationFile());
-					String response = "";
-					while (sc.hasNextLine()) {
-						response = response + sc.nextLine();
-					}
-					response = response.replace("%information%",
+					sendExchange(ex, FileManager.getInformationFile(), "%information%",
 							"You changed the local domain to " + requestRaw.replace("localdomain=", "") + ".");
-					ex.sendResponseHeaders(200, response.length());
-					OutputStream os = ex.getResponseBody();
-					os.write(response.getBytes());
-					os.close();
-					sc.close();
 				} else if (requestRaw.startsWith("username=")) {
 					Main.getConfiguration().setUsername(requestRaw.replace("username=", ""));
-					Scanner sc = new Scanner(FileManager.getInformationFile());
-					String response = "";
-					while (sc.hasNextLine()) {
-						response = response + sc.nextLine();
-					}
-					response = response.replace("%information%",
+					sendExchange(ex, FileManager.getInformationFile(), "%information%",
 							"You changed the username to " + requestRaw.replace("localdomain=", "") + ".");
-					ex.sendResponseHeaders(200, response.length());
-					OutputStream os = ex.getResponseBody();
-					os.write(response.getBytes());
-					os.close();
-					sc.close();
 				} else if (requestRaw.startsWith("password=")) {
 					Main.getConfiguration().setPassword(requestRaw.replace("password=", ""));
-					Scanner sc = new Scanner(FileManager.getInformationFile());
-					String response = "";
-					while (sc.hasNextLine()) {
-						response = response + sc.nextLine();
-					}
-					response = response.replace("%information%",
+					sendExchange(ex, FileManager.getInformationFile(), "%information%",
 							"You changed the password to " + requestRaw.replace("password=", "") + ".");
-					ex.sendResponseHeaders(200, response.length());
-					OutputStream os = ex.getResponseBody();
-					os.write(response.getBytes());
-					os.close();
-					sc.close();
 				} else if (requestRaw.startsWith("encryption=")) {
 					Main.getConfiguration().setEncryption(Integer.parseInt(requestRaw.replace("encryption=", "")));
-					Scanner sc = new Scanner(FileManager.getInformationFile());
-					String response = "";
-					while (sc.hasNextLine()) {
-						response = response + sc.nextLine();
-					}
 					if (Integer.parseInt(requestRaw.replace("encryption=", "")) == 0) {
-						response = response.replace("%information%", "You changed the encryption to http://.");
+						sendExchange(ex, FileManager.getInformationFile(), "%information%",
+								"You changed the encryption to http://.");
 					} else {
-						response = response.replace("%information%", "You changed the encryption to https://.");
+						sendExchange(ex, FileManager.getInformationFile(), "%information%",
+								"You changed the encryption to https://.");
 					}
-					ex.sendResponseHeaders(200, response.length());
-					OutputStream os = ex.getResponseBody();
-					os.write(response.getBytes());
-					os.close();
-					sc.close();
 				} else if (requestRaw.startsWith("port=")) {
 					Main.getConfiguration().setPort(Integer.parseInt(requestRaw.replace("port=", "")));
-					Scanner sc = new Scanner(FileManager.getInformationFile());
-					String response = "";
-					while (sc.hasNextLine()) {
-						response = response + sc.nextLine();
-					}
-					response = response.replace("%information%",
+					sendExchange(ex, FileManager.getInformationFile(), "%information%",
 							"You changed the encryption to " + requestRaw.replace("port=", "") + ".");
-					ex.sendResponseHeaders(200, response.length());
-					OutputStream os = ex.getResponseBody();
-					os.write(response.getBytes());
-					os.close();
-					sc.close();
 				} else if (requestRaw.startsWith("cupro=")) {
 					if (requestRaw.contains("val=")) {
 						String raw = requestRaw.replaceFirst("cupro=", "");
 						String[] keyVal = raw.split("val=");
 						if (keyVal.length != 2) {
-							Scanner sc = new Scanner(FileManager.getFailureFile());
-							String response = "";
-							while (sc.hasNextLine()) {
-								response = response + sc.nextLine();
-							}
-							response = response.replace("%error%",
-									Errors.CUSTOM_CONFIGURATION_INCORRECT_FORMAT.toString());
-							ex.sendResponseHeaders(200, response.length());
-							OutputStream os = ex.getResponseBody();
-							os.write(response.getBytes());
-							os.close();
-							sc.close();
+							sendFailure(ex, Errors.CUSTOM_CONFIGURATION_INCORRECT_FORMAT);
 						} else {
 							Main.getConfiguration().addProperty(keyVal[0], keyVal[1]);
 							Main.getConfiguration().save();
-							Scanner sc = new Scanner(FileManager.getInformationFile());
-							String response = "";
-							while (sc.hasNextLine()) {
-								response = response + sc.nextLine();
-							}
-							response = response.replace("%information%", "You added a custom property named '"
-									+ keyVal[0] + "' with the value '" + keyVal[1] + "'.");
-							ex.sendResponseHeaders(200, response.length());
-							OutputStream os = ex.getResponseBody();
-							os.write(response.getBytes());
-							os.close();
-							sc.close();
+							sendExchange(ex, FileManager.getInformationFile(), "%information%",
+									"You added a custom property named '" + keyVal[0] + "' with the value '" + keyVal[1]
+											+ "'.");
 						}
 					} else {
-						Scanner sc = new Scanner(FileManager.getFailureFile());
-						String response = "";
-						while (sc.hasNextLine()) {
-							response = response + sc.nextLine();
-						}
-						response = response.replace("%error%", Errors.CUSTOM_CONFIGURATION_MISSING_VALUE.toString());
-						ex.sendResponseHeaders(200, response.length());
-						OutputStream os = ex.getResponseBody();
-						os.write(response.getBytes());
-						os.close();
-						sc.close();
+						sendFailure(ex, Errors.CUSTOM_CONFIGURATION_MISSING_VALUE);
 					}
 				} else if (requestRaw.startsWith("getcupro=")) {
 					if (Main.getConfiguration().getProperties().containsKey(requestRaw.replaceFirst("getcupro=", ""))) {
-						Scanner sc = new Scanner(FileManager.getInformationFile());
-						String response = "";
-						while (sc.hasNextLine()) {
-							response = response + sc.nextLine();
-						}
-						response = response.replace("%information%",
-								"The property '" + requestRaw.replaceFirst("getcupro=", "") + "' has the value '" + Main.getConfiguration().getProperty(requestRaw.replaceFirst("getcupro=", "")) + "'.");
-						ex.sendResponseHeaders(200, response.length());
-						OutputStream os = ex.getResponseBody();
-						os.write(response.getBytes());
-						os.close();
-						sc.close();
+						sendExchange(ex, FileManager.getInformationFile(), "%information%",
+								"The property '" + requestRaw.replaceFirst("getcupro=", "") + "' has the value '"
+										+ Main.getConfiguration().getProperty(requestRaw.replaceFirst("getcupro=", ""))
+										+ "'.");
 					} else {
-						Scanner sc = new Scanner(FileManager.getFailureFile());
-						String response = "";
-						while (sc.hasNextLine()) {
-							response = response + sc.nextLine();
-						}
-						response = response.replace("%error%", Errors.CUSTOM_CONFIGURATION_NO_SUCH_PROPERTY.toString());
-						ex.sendResponseHeaders(200, response.length());
-						OutputStream os = ex.getResponseBody();
-						os.write(response.getBytes());
-						os.close();
-						sc.close();
+						sendFailure(ex, Errors.CUSTOM_CONFIGURATION_NO_SUCH_PROPERTY);
 					}
-				} else if(requestRaw.startsWith("remove=")) {
-					if(Main.getURLManager().containsURL(requestRaw.replaceFirst("remove=", ""))) {
+				} else if (requestRaw.startsWith("remove=")) {
+					if (Main.getURLManager().containsURL(requestRaw.replaceFirst("remove=", ""))) {
 						Main.getURLManager().removeURL(requestRaw.replaceFirst("remove=", ""));
-						Scanner sc = new Scanner(FileManager.getInformationFile());
-						String response = "";
-						while (sc.hasNextLine()) {
-							response = response + sc.nextLine();
-						}
-						response = response.replace("%information%",
-								"The URL '" + requestRaw.replaceFirst("remove=", "") + "' is now removed. It is no longer accessable.");
-						ex.sendResponseHeaders(200, response.length());
-						OutputStream os = ex.getResponseBody();
-						os.write(response.getBytes());
-						os.close();
-						sc.close();
+						sendExchange(ex, FileManager.getInformationFile(), "%information%",
+								"The URL '" + requestRaw.replaceFirst("remove=", "")
+										+ "' is now removed. It is no longer accessable.");
 					} else {
-						Scanner sc = new Scanner(FileManager.getFailureFile());
-						String response = "";
-						while (sc.hasNextLine()) {
-							response = response + sc.nextLine();
-						}
-						response = response.replace("%error%", Errors.NO_SUCH_URL.toString());
-						ex.sendResponseHeaders(200, response.length());
-						OutputStream os = ex.getResponseBody();
-						os.write(response.getBytes());
-						os.close();
-						sc.close();
+						sendFailure(ex, Errors.NO_SUCH_URL);
 					}
 				} else {
-					Scanner sc = new Scanner(FileManager.getFailureFile());
-					String response = "";
-					while (sc.hasNextLine()) {
-						response = response + sc.nextLine();
-					}
-					response = response.replace("%error%", Errors.CONFIGURATION_NO_PROPERTY_FOUND.toString());
-					ex.sendResponseHeaders(200, response.length());
-					OutputStream os = ex.getResponseBody();
-					os.write(response.getBytes());
-					os.close();
-					sc.close();
+					sendFailure(ex, Errors.CONFIGURATION_NO_PROPERTY_FOUND);
 				}
 			} else {
-				Scanner sc = new Scanner(FileManager.getFailureFile());
-				String response = "";
-				while (sc.hasNextLine()) {
-					response = response + sc.nextLine();
-				}
-				response = response.replace("%error%", Errors.NO_TARGET_FOUND.toString());
-				ex.sendResponseHeaders(200, response.length());
-				OutputStream os = ex.getResponseBody();
-				os.write(response.getBytes());
-				os.close();
-				sc.close();
+				sendFailure(ex, Errors.NO_TARGET_FOUND);
 			}
 		}
 	}
 
+	private static void sendExchange(HttpExchange ex, File f, String toReplace, String replacement) throws IOException {
+		Scanner sc = new Scanner(f);
+		String response = "";
+		while (sc.hasNextLine()) {
+			response = response + sc.nextLine();
+		}
+		response = response.replace(toReplace, replacement);
+		ex.sendResponseHeaders(200, response.length());
+		OutputStream os = ex.getResponseBody();
+		os.write(response.getBytes());
+		os.close();
+		sc.close();
+	}
+
+	private static void sendFailure(HttpExchange ex, Errors er) throws IOException {
+		Scanner sc = new Scanner(FileManager.getFailureFile());
+		String response = "";
+		while (sc.hasNextLine()) {
+			response = response + sc.nextLine();
+		}
+		response = response.replace("%error%", er.toString());
+		ex.sendResponseHeaders(200, response.length());
+		OutputStream os = ex.getResponseBody();
+		os.write(response.getBytes());
+		os.close();
+		sc.close();
+	}
 }
